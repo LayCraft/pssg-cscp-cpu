cb229bef6e6ce121e4fdca0d033aee7afe08d81c
Curtis <curtis.laycraft@zoho.com>
2019-09-12 10:51:21 -0700

    make contact information form stateless. move data handling into parent.

--- cpu-app/ClientApp/src/app/components/applicant-contact-information/applicant-contact-information.component.ts
+++ cpu-app/ClientApp/src/app/components/applicant-contact-information/applicant-contact-information.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input, ViewChild } from '@angular/core';
 import { ContactInformation, iContactInformation } from '../../classes/contact-information.class';
 import { emailValidRegex, phoneValidRegex, postalCodeValidRegex } from '../../constants/validators';
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, NgForm } from '@angular/forms';
 import { iCountry, COUNTRIES_ADDRESS_2 } from '../../constants/country-list';
 import { Address } from '../../classes/address.class';
-// import { BoilerplateService } from '../../services/boilerplate.service';
 
 @Component({
   selector: 'app-applicant-contact-information',
@@ -12,32 +11,26 @@ import { Address } from '../../classes/address.class';
   styleUrls: ['./applicant-contact-information.component.scss']
 })
 export class ApplicantContactInformationComponent implements OnInit {
-  // this would be some kind of object or string we can look up the contact information from an API.
-  // @Input() organizationId = null;
-
+  // a viewchild to check the validity of the template form
+  @ViewChild(NgForm) ciForm;
   // input a contact information properties to create this form
   @Input() contactInformation: iContactInformation;
   // output a contact on change
   @Output() contactInformationChange = new EventEmitter<ContactInformation>();
   // is the contents of the form valid?
   @Output() valid = new EventEmitter<boolean>();
-  // @Output() save = new EventEmitter<iContactInformation>();
 
   // patterns
   emailValidRegex = emailValidRegex;
   phoneValidRegex = phoneValidRegex;
   postalCodeValidRegex = postalCodeValidRegex;
-
   // form state
   hasMailingAddress = false;
   country: iCountry;
-
   // form model
   contactInformationForm: ContactInformation;
 
-  constructor(
-    // private boilerplateService: BoilerplateService,
-  ) { }
+  constructor() { }
 
   ngOnInit() {
     this.contactInformationForm = new ContactInformation();
@@ -45,32 +38,15 @@ export class ApplicantContactInformationComponent implements OnInit {
     this.country = COUNTRIES_ADDRESS_2.Canada;
     // initialize the contact information
     this.contactInformationForm = new ContactInformation(this.contactInformation);
-
-    // if (this.contactInformation) {
-    //   // if the organization ID is passed in then get it from the boilerplate service
-    //   this.boilerplateService.getOrganizationBoilerplate(this.organizationId).subscribe((info: iContactInformation) => {
-    //     // when the component loads make a new working contact information object to do the form work in
-    //     this.contactInformation = new ContactInformation(info);
-
-    //     // TODO: In the future we can convert the dynamics junk data into a useful collection.
-    //     // const ci: ContactInformation = new ContactInformation();
-    //     // ci.fromDynamics(info);
-    //     // this.contactInformation = ci;
-
-    //     // if there is something returned in the mailing address then we should show the section
-    //     if (info.mailingAddress) {
-    //       this.hasMailingAddress = true;
-    //     }
-    //   });
-    // } else {
-    //   // no included organization ID? make a blank form.
-    //   this.contactInformation = new ContactInformation();
-    // }
+    // now that the form is initialized we emit it to send the validity to the parent. Otherwise we have to wait for the user to change something.
+    this.onInput();
   }
 
   // if the supplied information changes reinitialize it into the form
   ngOnChange(change: iContactInformation) {
     this.contactInformationForm = new ContactInformation(change);
+    // now that the form is initialized we emit it to send the validity to the parent. Otherwise we have to wait for the user to change something.
+    this.onInput();
   }
 
   // form helpers. Validity hints and hide/show toggles
@@ -84,17 +60,10 @@ export class ApplicantContactInformationComponent implements OnInit {
     }
   }
 
-  // onSubmit(valid: boolean) {
-  //   // if the form is valid we emit.
-  //   if (valid) {
-  //     this.save.emit(this.contactInformationForm);
-  //   } else {
-  //     alert('This form is not valid.');
-  //   }
-  // }
-
-  onChange(valid: boolean) {
-    this.valid.emit(valid);
+  onInput() {
+    // is this valid? Emit it boolean
+    this.valid.emit(this.ciForm.valid);
+    // emit the form
     this.contactInformationChange.emit(this.contactInformationForm);
   }
 }
--- cpu-app/ClientApp/src/app/pages/organization-profile-page/organization-profile-page.component.ts
+++ cpu-app/ClientApp/src/app/pages/organization-profile-page/organization-profile-page.component.ts
@@ -24,6 +24,27 @@ export class OrganizationProfilePageComponent implements OnInit {
         // save the boilerplate information into a place that we can edit it
         this.contactInformation = ci;
       });
+
+    // if (this.contactInformation) {
+    //   // if the organization ID is passed in then get it from the boilerplate service
+    //   this.boilerplateService.getOrganizationBoilerplate(this.organizationId).subscribe((info: iContactInformation) => {
+    //     // when the component loads make a new working contact information object to do the form work in
+    //     this.contactInformation = new ContactInformation(info);
+
+    //     // TODO: In the future we can convert the dynamics junk data into a useful collection.
+    //     // const ci: ContactInformation = new ContactInformation();
+    //     // ci.fromDynamics(info);
+    //     // this.contactInformation = ci;
+
+    //     // if there is something returned in the mailing address then we should show the section
+    //     if (info.mailingAddress) {
+    //       this.hasMailingAddress = true;
+    //     }
+    //   });
+    // } else {
+    //   // no included organization ID? make a blank form.
+    //   this.contactInformation = new ContactInformation();
+    // }
   }
   onSave() {
     this.boilerplateService.setOrganizationBoilerplate(this.organizationId, this.contactInformation).subscribe(
