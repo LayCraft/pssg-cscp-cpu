f9d6fa7206e7821d49cf17ff35c784283965b789
Curtis <curtis.laycraft@zoho.com>
2019-09-13 16:11:21 -0700

    remove old rxjs calls

--- cpu-app/ClientApp/src/app/app.component.ts
+++ cpu-app/ClientApp/src/app/app.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { VersionInfoDataService } from './services/version-info-data.service';
-import { User } from './models/user.model';
+import { Component, OnInit } from '@angular/core';
+
 import { VersionInfo } from './models/version-info.model';
 import { isDevMode } from '@angular/core';
-import { MatDialog } from '@angular/material';
-import 'rxjs/add/operator/filter';
-import { VersionInfoDialog } from './shared/components/version-info/version-info.component';
+
 
 @Component({
   selector: 'app-root',
