abe63df7c9645aa37b418285eb6a171c9059691d
Curtis <curtis.laycraft@zoho.com>
2019-09-13 14:30:35 -0700

    form structure

--- cpu-app/ClientApp/src/app/components/administrative-information/administrative-information.component.ts
+++ cpu-app/ClientApp/src/app/components/administrative-information/administrative-information.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-administrative-information',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrative-information.component.scss']
 })
 export class AdministrativeInformationComponent implements OnInit {
+  // a viewchild to check the validity of the template form
+  @ViewChild(NgForm) aiForm;
+  // input a contact information properties to create this form
+  @Input() administrativeInformation: any;
+  // output a contact on change
+  @Output() administrativeInformationChange = new EventEmitter<any>();
+  // is the contents of the form valid?
+  @Output() valid = new EventEmitter<boolean>();
 
+  administrativeInfoForm;
   constructor() { }
 
   ngOnInit() {
   }
-
+  onInput() {
+    // is this valid? Emit it boolean
+    this.valid.emit(this.aiForm.valid);
+    // emit the form
+    this.administrativeInformationChange.emit(this.administrativeInfoForm);
+  }
 }
