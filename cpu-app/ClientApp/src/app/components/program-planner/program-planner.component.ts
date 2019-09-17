f974acc4fd68583edbcb3015f43236149a305ccc
Curtis <curtis.laycraft@zoho.com>
2019-09-17 09:57:12 -0700

    set program contact

--- cpu-app/ClientApp/src/app/components/program-planner/program-planner.component.ts
+++ cpu-app/ClientApp/src/app/components/program-planner/program-planner.component.ts
@@ -1,8 +1,8 @@
 import { NgForm, AbstractControl } from '@angular/forms';
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
-import { ProgramInformation, iProgramMeta, iProgramInformation } from '../../classes/program-information.class';
-import { BoilerplateService } from '../../services/boilerplate.service';
-import { ProgramInformationService } from '../../services/program-information.service';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { ProgramInformation, iProgramMeta } from '../../classes/program-information.class';
+
+import { ContactInformation } from '../../classes/contact-information.class';
 @Component({
   selector: 'app-program-planner',
   templateUrl: './program-planner.component.html',
@@ -16,6 +16,9 @@ export class ProgramPlannerComponent implements OnInit {
 
   // the form model
   programInformationForm: ProgramInformation;
+  differentProgramContact: boolean = false;
+  programContactForm: ContactInformation;
+
 
   currentTab: string;
   tabs: string[];
@@ -29,15 +32,21 @@ export class ProgramPlannerComponent implements OnInit {
 
   ngOnInit() {
     this.programInformationForm = new ProgramInformation();
+    this.programContactForm = new ContactInformation();
     // initialize the program information if it is supplied else make a new object
     // this.programInformationService.getProgramInformation(this.programMeta.organizationId, this.programMeta.programId).subscribe((p: iProgramInformation) => {
     //   this.programInformationForm = new ProgramInformation(p);
     // });
-
   }
 
   // form helpers. Validity hints and hide/show toggles
   showValidFeedback(control: AbstractControl): boolean { return !(control.valid && (control.dirty || control.touched)) }
   showInvalidFeedback(control: AbstractControl): boolean { return !(control.invalid && (control.dirty || control.touched)) }
   onInput() { }
+
+  showProgramContact() {
+    // Whether turned off or on we wipe out the contact information
+    this.programContactForm = new ContactInformation();
+    this.differentProgramContact = !this.differentProgramContact;
+  }
 }
