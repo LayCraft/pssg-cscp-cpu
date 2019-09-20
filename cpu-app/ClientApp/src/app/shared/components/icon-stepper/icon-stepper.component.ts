5cf641a6ee7e5d0861d1b32c5d0d4402ea4708b2
Curtis <curtis.laycraft@zoho.com>
2019-09-20 09:16:43 -0700

    make icon stepper component

---
+++ cpu-app/ClientApp/src/app/shared/components/icon-stepper/icon-stepper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-stepper',
+  templateUrl: './icon-stepper.component.html',
+  styleUrls: ['./icon-stepper.component.scss']
+})
+export class IconStepperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- cpu-app/ClientApp/src/app/test/test.component.ts
+++ cpu-app/ClientApp/src/app/test/test.component.ts
@@ -1,20 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-class DummyForm {
-  postal: string;
-}
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.scss']
+	selector: 'app-test',
+	templateUrl: './test.component.html',
+	styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
-  form: DummyForm;
-  postalPattern = /^[A-Za-z][0-9][A-Za-z][ ]?[0-9][A-Za-z][0-9]$/;
-  constructor() {
-    this.form = new DummyForm();
-  }
+	constructor() { }
 
-  ngOnInit() {
-
-  }
+	ngOnInit() { }
 }
