c68b79b77e5c15cefb48d30dcaec9215842d6a6b
Curtis <curtis.laycraft@zoho.com>
2019-09-25 11:47:51 -0700

    stepper element to program overview

--- cpu-app/ClientApp/src/app/authenticated/pages/budget-page/program-overview/program-overview.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/pages/budget-page/program-overview/program-overview.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { iStepperElement } from 'src/app/core/models/stepper-element';
 
 @Component({
-  selector: 'app-program-overview',
-  templateUrl: './program-overview.component.html',
-  styleUrls: ['./program-overview.component.scss']
+	selector: 'app-program-overview',
+	templateUrl: './program-overview.component.html',
+	styleUrls: ['./program-overview.component.scss']
 })
 export class ProgramOverviewComponent implements OnInit {
 
-  constructor() { }
+	@Input() stepperElement: iStepperElement;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
