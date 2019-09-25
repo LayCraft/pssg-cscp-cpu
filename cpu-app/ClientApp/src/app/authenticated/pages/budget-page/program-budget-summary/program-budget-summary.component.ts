8a4445726d05d55cdefdfebb7f0faab939c35370
Curtis <curtis.laycraft@zoho.com>
2019-09-25 11:50:11 -0700

    add input to budget summary

--- cpu-app/ClientApp/src/app/authenticated/pages/budget-page/program-budget-summary/program-budget-summary.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/pages/budget-page/program-budget-summary/program-budget-summary.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { iStepperElement } from 'src/app/core/models/stepper-element';
 
 @Component({
-  selector: 'app-program-budget-summary',
-  templateUrl: './program-budget-summary.component.html',
-  styleUrls: ['./program-budget-summary.component.scss']
+	selector: 'app-program-budget-summary',
+	templateUrl: './program-budget-summary.component.html',
+	styleUrls: ['./program-budget-summary.component.scss']
 })
 export class ProgramBudgetSummaryComponent implements OnInit {
+	@Input() stepperElement: iStepperElement;
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
