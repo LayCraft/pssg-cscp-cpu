4e6e9a68e663cf66aecc47e1a6a67f4492429d6a
Curtis <curtis.laycraft@zoho.com>
2019-09-26 10:19:53 -0700

    city field

--- cpu-app/ClientApp/src/app/authenticated/components/address/address.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/components/address/address.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { iAddress } from 'src/app/core/models/address.class';
 import { NgxSubFormComponent, Controls, subformComponentProviders } from 'ngx-sub-form';
 import { FormControl, AbstractControl } from '@angular/forms';
+import { COUNTRIES_ADDRESS_2, iCountry } from 'src/app/core/constants/country-list';
 
 
 @Component({
@@ -10,7 +11,10 @@ import { FormControl, AbstractControl } from '@angular/forms';
 	styleUrls: ['./address.component.scss'],
 	providers: subformComponentProviders(AddressComponent),
 })
-export class AddressComponent extends NgxSubFormComponent<iAddress> {
+export class AddressComponent extends NgxSubFormComponent<iAddress> implements OnInit {
+	required: boolean = true;
+	country: iCountry; // currently selected country
+
 	// this is not a form only part of a form. This needs 
 	protected getFormControls(): Controls<iAddress> {
 		return {
@@ -25,4 +29,9 @@ export class AddressComponent extends NgxSubFormComponent<iAddress> {
 	// form helpers. Validity hints and hide/show toggles
 	showValidFeedback(control: AbstractControl): boolean { return !(control.valid && (control.dirty || control.touched)) }
 	showInvalidFeedback(control: AbstractControl): boolean { return !(control.invalid && (control.dirty || control.touched)) }
+
+	ngOnInit() {
+		// set country to canada
+		this.country = COUNTRIES_ADDRESS_2.Canada;
+	}
 }
