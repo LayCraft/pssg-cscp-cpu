32903619b8e044e1ef243054e13260c2e872b9c8
Curtis <curtis.laycraft@zoho.com>
2019-10-01 14:40:20 -0700

    add some default items

--- cpu-app/ClientApp/src/app/authenticated/modules/budget-proposal/program-budget/program-budget.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/modules/budget-proposal/program-budget/program-budget.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { iStepperElement } from 'src/app/core/models/stepper-element';
 import { RevenueSource, iRevenueSource } from 'src/app/core/models/revenue-source.class';
+import { iExpenseItem } from 'src/app/core/models/budget-proposal.class';
 
 @Component({
 	selector: 'app-program-budget',
@@ -13,6 +14,18 @@ export class ProgramBudgetComponent implements OnInit {
 	tabs: string[];
 
 	revenueSources: iRevenueSource[] = [];
+	expenseItems: iExpenseItem[] = [];
+	defaultExpenseItems: iExpenseItem[] = [
+		{ itemName: 'Program - related' } as iExpenseItem,
+		{ itemName: 'Program - related office supplies / software' } as iExpenseItem,
+		{ itemName: 'Program - related travel' } as iExpenseItem,
+		{ itemName: 'Utilities' } as iExpenseItem,
+		{ itemName: 'Phone' } as iExpenseItem,
+		{ itemName: 'Staff training and associated travel' } as iExpenseItem,
+		{ itemName: 'Resource materials / printing costs' } as iExpenseItem,
+		{ itemName: 'Volunteer appreciation / honorariums' } as iExpenseItem,
+		{ itemName: 'Property maintenance' } as iExpenseItem,
+	];
 
 	constructor(
 	) {
@@ -24,3 +37,13 @@ export class ProgramBudgetComponent implements OnInit {
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
