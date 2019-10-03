38838bc9113a53d1d3793edcdf37acc5fd995b28
Curtis <curtis.laycraft@zoho.com>
2019-10-03 09:43:45 -0700

    person editor basic setup

--- cpu-app/ClientApp/src/app/modules/personnel/person-editor/person-editor.component.ts
+++ cpu-app/ClientApp/src/app/modules/personnel/person-editor/person-editor.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { iPerson, Person } from 'src/app/core/models/person.class';
 
 @Component({
-  selector: 'app-person-editor',
-  templateUrl: './person-editor.component.html',
-  styleUrls: ['./person-editor.component.scss']
+	selector: 'app-person-editor',
+	templateUrl: './person-editor.component.html',
+	styleUrls: ['./person-editor.component.scss']
 })
 export class PersonEditorComponent implements OnInit {
+	@Input() person: iPerson;
+	@Output() personChange = new EventEmitter<iPerson>();
 
-  constructor() { }
+	// main collection
+	personForm: Person;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+		this.personForm = new Person(this.person);
+	}
+	onChanges() {
+		this.personChange.emit(this.personForm);
+	}
 
 }
