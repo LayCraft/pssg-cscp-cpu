d98d955dcff9fbcfa6778d3bd93bc0154aff24aa
Curtis <curtis.laycraft@zoho.com>
2019-10-07 11:19:46 -0700

    add program application subform

---
+++ cpu-app/ClientApp/src/app/modules/program-application/program-application/program-application.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-program-application',
+  templateUrl: './program-application.component.html',
+  styleUrls: ['./program-application.component.scss']
+})
+export class ProgramApplicationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
