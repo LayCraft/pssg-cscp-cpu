7e4aca7b847df467b1101923d3db3a57455a9d39
Curtis <curtis.laycraft@zoho.com>
2019-10-16 08:22:18 -0700

    notification component

--- cpu-app/ClientApp/src/app/shared/notification-banner/notification-banner.component.ts
+++ cpu-app/ClientApp/src/app/shared/notification-banner/notification-banner.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-notification-banner',
   templateUrl: './notification-banner.component.html',
-  styleUrls: ['./notification-banner.component.css']
+  styleUrls: ['./notification-banner.component.css'],
+  animations: [
+    // the fade-in/fade-out animation.
+    trigger('fadeAnimation', [
+
+      // the "in" style determines the "resting" state of the element when it is visible.
+      state('in', style({ opacity: 1 })),
+
+      // fade in when created. this could also be written as transition('void => *')
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(400)
+      ]),
+
+      // fade out when destroyed. this could also be written as transition('void => *')
+      transition(':leave',
+        animate(600, style({ opacity: 0 })))
+    ])
+  ],
 })
 export class NotificationBannerComponent implements OnInit {
 
-  constructor() { }
+  staticAlertClosed = false;
+  successMessage: string;
+  currentNotifications: Notification[];
 
-  ngOnInit() {
+  constructor(
+    // private notificationQueueService: NotificationQueueService,
+  ) { }
+
+  ngOnInit(): void {
+    // this.notificationQueueService.notificationQueue.subscribe((notifications: Notification[]) => {
+    //   this.currentNotifications = notifications;
+    // });
+  }
+
+  close(notification: Notification) {
+    // expire the notification
+    // this.notificationQueueService.expireNotification(notification.identifier);
   }
 
 }
