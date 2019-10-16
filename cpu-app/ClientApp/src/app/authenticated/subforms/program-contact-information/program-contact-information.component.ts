49ec84d1bc22e582bb204403cea4abac1edb2b99
Curtis <curtis.laycraft@zoho.com>
2019-10-16 10:49:58 -0700

    program contact information

--- cpu-app/ClientApp/src/app/authenticated/subforms/program-contact-information/program-contact-information.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/subforms/program-contact-information/program-contact-information.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Person, iPerson } from '../../../core/models/person.class';
+import { iContactInformation } from '../../../core/models/contact-information.class';
 
 @Component({
   selector: 'app-program-contact-information',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgramContactInformationComponent implements OnInit {
 
-  constructor() { }
+  // this collects all of the information for all programs so we are actually collecting parent data
+  // we should patch it into the data on change
+  @Input() contactInformation: iContactInformation;
+  @Output() contactInformationChange = new EventEmitter<iContactInformation>();
+  @Input() required = true;
+  @Input() title = 'Primary Program Contact Information';
+
+  // formHelper = new FormHelper();
+  contactInformationForm: FormGroup;
+  persons: Person[] = [];
+  hasBoardOfDirectors: boolean = false;
+
+  executive: iPerson;
+  boardContact: iPerson;
+
+  constructor(
+    // private boilerplateService: BoilerplateService,
+    // private personService: PersonService,
+  ) { }
 
   ngOnInit() {
+    // get the boilerplate from the service
+    // this.boilerplateService.getOrganizationBoilerplate('foobazqux').subscribe(ci => {
+    // 	this.contactInformationForm = new FormGroup({
+    // 		'contactInformation': new FormControl('', Validators.required)
+    // 	});
+    // 	this.contactInformationForm.controls['contactInformation'].setValue(ci);
+    // });
+    // set the persons list
+    // this.personService.getPersons('borkityfoobar').subscribe(p => this.persons = p);
   }
-
+  onInput() {
+    console.log(this.contactInformationForm.value['contactInformation'])
+    // emit the information
+    this.contactInformationChange.emit(this.contactInformationForm.value['contactInformation']);
+  }
+  pickExecutive(event: iPerson) {
+    this.executive = event;
+  }
+  pickBoardContact(event: iPerson) {
+    this.boardContact = event;
+  }
+  cleanBoardContact() {
+    // if the board of directors is false we need to remove the object.
+    if (!this.hasBoardOfDirectors) {
+      this.boardContact = null;
+    }
+  }
 }
