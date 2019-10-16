5820a18db056d1f91f28b77ee0df92d42d71b329
Curtis <curtis.laycraft@zoho.com>
2019-10-16 13:31:36 -0700

    add test button

--- cpu-app/ClientApp/src/app/test/test.component.ts
+++ cpu-app/ClientApp/src/app/test/test.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { retry, catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-test',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  // store the results
+  test: any;
 
-  ngOnInit() {
+  // this should query the test api
+  apiUrl = 'api/justice/test';
+
+  constructor(private http: HttpClient) { }
+  ngOnInit() { }
+  get headers(): HttpHeaders {
+    return new HttpHeaders({ 'Content-Type': 'application/json' });
   }
-
+  getTest(): Observable<any> {
+    return this.http.get<any>(this.apiUrl, { headers: this.headers }).pipe(
+      retry(3),
+      catchError(this.handleError)
+    );
+  }
+  protected handleError(err): Observable<never> {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = err.error.message;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Backend returned code ${err.status}, body was: ${err.message}`;
+    }
+    return throwError(errorMessage);
+  }
+  onClick() {
+    this.getTest().subscribe(
+      x => this.test = x,
+      err => console.log(err),
+      () => console.log('Finished')
+    );
+  }
 }
