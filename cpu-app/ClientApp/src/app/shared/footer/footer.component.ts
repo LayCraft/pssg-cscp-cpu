cad7253b3161ecc94a503c7d34a0f84aedad779d
Curtis <curtis.laycraft@zoho.com>
2019-10-16 08:19:00 -0700

    footer

--- cpu-app/ClientApp/src/app/shared/footer/footer.component.ts
+++ cpu-app/ClientApp/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
-
+  homeButton() {
+    // this is done without a routerlink because you will want to route the user back to a place
+    // that is appropriate for their role. So check their logged in state and etc before deciding which route they go to.
+    this.router.navigate(['']);
+  }
 }
