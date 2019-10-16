484adbf7836ca954bfee24b2a5158fd463a8cf51
Curtis <curtis.laycraft@zoho.com>
2019-10-16 13:09:46 -0700

    log in and out functionality

--- cpu-app/ClientApp/src/app/shared/header/header.component.ts
+++ cpu-app/ClientApp/src/app/shared/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../../core/services/authentication.service';
+import { NotificationQueueService } from '../../core/services/notification-queue.service';
 
 @Component({
   selector: 'app-header',
@@ -11,34 +13,32 @@ export class HeaderComponent implements OnInit {
   title: string = 'Victims Services Community Programs Unit';
   constructor(
     private router: Router,
-    // private authenticationService: AuthenticationService,
-    // private notificationQueueService: NotificationQueueService,
+    private authenticationService: AuthenticationService,
+    private notificationQueueService: NotificationQueueService,
   ) { }
 
   ngOnInit() {
-    // this.authenticationService.user.subscribe(u => {
-    // 	if (this.user === '') {
-    // 		// this is the first page visit. Just skip the notifications.
-    // 		// If it were null it came after a logout.
-    // 	} else if (!u) {
-    // 		// User signed out because behaviour subject was blank
-    // 		this.notificationQueueService.addNotification('User has logged out.', 'warning');
-    // 		// Go back home!m
-    // 		this.router.navigate(['']);
-    // 	} else {
-    // 		// User signed in because behaviour subject was not blank
-    // 		this.notificationQueueService.addNotification(`${u} has been logged in successfully.`, 'success');
-    // 	}
-    // 	this.user = u;
-    // });
+    this.authenticationService.user.subscribe(u => {
+      if (this.user === '') {
+        // this is the first page visit. Just skip the notifications.
+        // If it were null it came after a logout.
+      } else if (!u) {
+        // User signed out because behaviour subject was blank
+        this.notificationQueueService.addNotification('User has logged out.', 'warning');
+        // Go back home!m
+        this.router.navigate(['']);
+      } else {
+        // User signed in because behaviour subject was not blank
+        this.notificationQueueService.addNotification(`${u} has been logged in successfully.`, 'success');
+      }
+      this.user = u;
+    });
   }
   login() {
-    // this.authenticationService.login();
-    alert('Log in!');
+    this.authenticationService.login();
   }
   logout() {
-    // this.authenticationService.logout();
-    alert('Log out!');
+    this.authenticationService.logout();
   }
   homeButton() {
     // this is done without a routerlink because you will want to route the user back to a place
