1e641632c64d7c543a279e3abe34cea1a9544082
Curtis <curtis.laycraft@zoho.com>
2019-10-23 11:03:13 -0700

    state holder component

--- cpu-app/ClientApp/src/app/shared/state-holder/state-holder.component.ts
+++ cpu-app/ClientApp/src/app/shared/state-holder/state-holder.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { StateService } from '../../core/services/state.service';
 
 @Component({
   selector: 'app-state-holder',
   templateUrl: './state-holder.component.html',
   styleUrls: ['./state-holder.component.css']
 })
-export class StateHolderComponent implements OnInit {
-
-  constructor() { }
+export class StateHolderComponent implements OnInit, OnDestroy {
+  constructor(
+    private stateService: StateService
+  ) { }
 
   ngOnInit() {
-    // alert('State')
+    // this quietly watches the application state and makes sure that something is subscribed to the stateservice if someone is logged in.
+    this.stateService.main.subscribe();
+    console.log("Logged in");
+  }
+  ngOnDestroy() {
+    console.log("Logged out");
   }
 
 }
