e9d0c10ce05062c97233e03de39f21b114b854cc
Curtis <curtis.laycraft@zoho.com>
2019-10-24 10:29:16 -0700

    flow data into ministry contact box

--- cpu-app/ClientApp/src/app/authenticated/components/ministry-contact-box/ministry-contact-box.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/components/ministry-contact-box/ministry-contact-box.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StateService } from '../../../core/services/state.service';
 import { iMinistryUser } from '../../../core/models/ministry-user';
+import { nameAssemble } from '../../../core/constants/name-assemble';
 
 @Component({
   selector: 'app-ministry-contact-box',
@@ -9,6 +10,7 @@ import { iMinistryUser } from '../../../core/models/ministry-user';
 })
 export class MinistryContactBoxComponent implements OnInit {
   ministryUser: iMinistryUser;
+  nameAssemble = nameAssemble;
   constructor(
     private stateService: StateService,
   ) { }
