77cd3b3047ecce91362241987b4118ab205e5f85
Curtis <curtis.laycraft@zoho.com>
2019-11-07 08:49:36 -0800

    update data type contact

--- cpu-app/Models/DynamicsCrmContact.cs
+++ cpu-app/Models/DynamicsCrmContact.cs
@@ -23,6 +23,7 @@ namespace Gov.Cscp.VictimServices.Public.Models
 		public string lastname { get; set; }
 		public string middlename { get; set; }
 		public string mobilephone { get; set; }
+		public string odatatype = "#Microsoft.Dynamics.CRM.contact";
 		public string vsd_bceid { get; set; }
 	}
 }
