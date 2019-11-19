8242204f737c0c28b10f45d61f3cb70e99a256a6
Curtis <curtis.laycraft@zoho.com>
2019-11-19 14:45:33 -0800

    page stepper works monthly statistics

--- cpu-app/ClientApp/src/app/authenticated/status-report/status-report.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/status-report/status-report.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { iContract } from '../../core/models/contract';
+import { IconStepperService, iStepperElement } from '../../shared/icon-stepper/icon-stepper.service';
+import { StateService } from '../../core/services/state.service';
 
 @Component({
   selector: 'app-status-report',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./status-report.component.css']
 })
 export class StatusReportComponent implements OnInit {
-
-  constructor() { }
+  contract: iContract;
+  // used for the stepper component
+  stepperElements: iStepperElement[];
+  currentStepperElement: iStepperElement;
+  constructor(
+    private stepperService: IconStepperService,
+    private stateService: StateService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(p => {
+      // collect the contract from the route
+      const contractId = p['contractId'];
+      // don't subscribe because this is only used at page load to collect meta
+      const contracts: iContract[] = this.stateService.main.getValue().contracts;
+      // isolate the correct contract
+      for (let contract of contracts) {
+        if (contract.contractId == contractId) {
+          this.contract = contract;
+        }
+      }
+      // clear all of the old stepper elements
+      this.stepperService.reset();
+      this.constructDefaultstepperElements();
+    });
+    // stay in sync with
+    this.stepperService.currentStepperElement.subscribe(e => this.currentStepperElement = e);
+    this.stepperService.stepperElements.subscribe(e => this.stepperElements = e);
   }
-
+  isCurrentStepperElement(item: iStepperElement): boolean {
+    if (item.id === this.currentStepperElement.id) {
+      return true;
+    }
+    return false;
+  }
+  constructDefaultstepperElements() {
+    // write the default beginning
+    [
+      {
+        itemName: 'Page 1',
+        formState: 'untouched',
+        object: null,
+        discriminator: 'contact_information',
+      },
+      {
+        itemName: 'Page 2',
+        formState: 'untouched',
+        object: null,
+        discriminator: 'administrative_information',
+      }
+    ].forEach((f: iStepperElement) => {
+      this.stepperService.addStepperElement(f.object, f.itemName, f.formState, f.discriminator);
+    });
+  }
 }
