dc7d0a46312090d1c14ffc77a543bc5ff05add81
Curtis <curtis.laycraft@zoho.com>
2019-11-27 14:04:57 -0800

    add basic transmogrifier

---
+++ cpu-app/ClientApp/src/app/core/models/transmogrifier-expense-report.class.ts
@@ -0,0 +1,8 @@
+import { iDynamicsScheduleGResponse } from "./dynamics-schedule-g-response";
+
+export class TransmogrifierExpenseReport {
+  foo: any;
+  constructor(g: iDynamicsScheduleGResponse) {
+    this.foo = g;
+  }
+}
