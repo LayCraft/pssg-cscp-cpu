9bbc4d0a4a02a8656213b8335953867a9584e579
Curtis <curtis.laycraft@zoho.com>
2019-11-29 11:53:30 -0800

    expense report get from dynamics

--- cpu-app/ClientApp/src/app/authenticated/expense-report/expense-report.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/expense-report/expense-report.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ExpenseReportService } from '../../core/services/expense-report.service';
+import { StateService } from '../../core/services/state.service';
+import { TransmogrifierExpenseReport } from '../../core/models/transmogrifier-expense-report.class';
 
 @Component({
   selector: 'app-expense-report',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseReportComponent implements OnInit {
 
-  constructor() { }
+  // expense report
+  er: TransmogrifierExpenseReport;
+
+  constructor(
+    private expenseReportService: ExpenseReportService,
+    private stateService: StateService,
+  ) { }
 
   ngOnInit() {
-  }
 
+    this.expenseReportService.getScheduleG("e480dbe7-a910-ea11-b810-005056830319").subscribe(g =>
+      this.er = new TransmogrifierExpenseReport(g));
+  }
 }
