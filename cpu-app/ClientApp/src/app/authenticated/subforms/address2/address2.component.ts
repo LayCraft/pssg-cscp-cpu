0fd13695cada17d612a0e5f71c09379af59e7265
Curtis <curtis.laycraft@zoho.com>
2019-12-16 12:24:47 -0800

    mailing and main address move

--- cpu-app/ClientApp/src/app/authenticated/subforms/address2/address2.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/subforms/address2/address2.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormHelper } from '../../../core/form-helper';
 import { iAddress, Address } from '../../../core/models/address.class';
+import { COUNTRIES_ADDRESS_2 } from '../../../core/constants/country-list';
 
 @Component({
   selector: 'app-address2',
@@ -13,10 +14,16 @@ export class Address2Component implements OnInit {
   @Input() address: iAddress;
   @Output() addressChange = new EventEmitter<iAddress>();
 
-  constructor() { }
+  countries;
+  country;
+
+  constructor() {
+    this.countries = COUNTRIES_ADDRESS_2;
+    this.country = this.address && this.address.country ? COUNTRIES_ADDRESS_2[this.address.country] : COUNTRIES_ADDRESS_2['Canada'];
+  }
 
   ngOnInit() {
-    this.address = new Address();
+    this.address = this.address ? new Address(this.address) : new Address();
   }
   onChange() {
     this.addressChange.emit(this.address);
