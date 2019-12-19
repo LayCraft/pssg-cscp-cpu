e8fcfe5f1a822699fc6c044c1722f9fe7b3670d0
Curtis <curtis.laycraft@zoho.com>
2019-12-19 09:40:18 -0800

    paginate basic on review

--- cpu-app/ClientApp/src/app/authenticated/subforms/review-application/review-application.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/subforms/review-application/review-application.component.ts
@@ -1,6 +1,7 @@
 import * as moment from 'moment';
 import { Component, OnInit, Input } from '@angular/core';
 import { TransmogrifierProgramApplication } from '../../../core/models/transmogrifier-program-application.class';
+import { iProgramApplication } from '../../../core/models/program-application.class';
 
 @Component({
   selector: 'app-review-application',
@@ -9,8 +10,27 @@ import { TransmogrifierProgramApplication } from '../../../core/models/transmogr
 })
 export class ReviewApplicationComponent implements OnInit {
   @Input() tpa: TransmogrifierProgramApplication;
+
+  currentTab: string = 'Application Information';
+  tabs: string[] = ['Application Information'];
+
   constructor() { }
 
-  ngOnInit() { }
-
+  ngOnInit() {
+    this.tpa.programApplications.forEach((p: iProgramApplication) => { this.tabs.push(p.name) });
+  }
+  nextPage() {
+    const index = this.tabs.indexOf(this.currentTab);
+    if (!(index >= this.tabs.length - 1)) {
+      this.currentTab = this.tabs[index + 1];
+      window.scrollTo(0, 0);
+    }
+  }
+  prevPage() {
+    const index = this.tabs.indexOf(this.currentTab);
+    if (index > 0) {
+      this.currentTab = this.tabs[index - 1];
+      window.scrollTo(0, 0);
+    }
+  }
 }
