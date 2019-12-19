62c63407a1b7e99490a0666323ea437a70c1bcf6
Curtis <curtis.laycraft@zoho.com>
2019-12-19 14:35:28 -0800

    sort app module

--- cpu-app/ClientApp/src/app/app.module.ts
+++ cpu-app/ClientApp/src/app/app.module.ts
@@ -1,89 +1,89 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { Address2Component } from './authenticated/subforms/address2/address2.component';
+import { AddressComponent } from './authenticated/subforms/address/address.component';
+import { AdministrativeInformationComponent } from './authenticated/subforms/administrative-information/administrative-information.component';
+import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { SharedModule } from './shared/shared.module';
-import { DashboardComponent } from './authenticated/dashboard/dashboard.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BudgetProposalComponent } from './authenticated/budget-proposal/budget-proposal.component';
-import { ExpenseReportComponent } from './authenticated/expense-report/expense-report.component';
-import { ProgramApplicationComponent } from './authenticated/program-application/program-application.component';
-import { StatusReportComponent } from './authenticated/status-report/status-report.component';
-import { PersonnelComponent } from './authenticated/personnel/personnel.component';
-import { ProfileComponent } from './authenticated/profile/profile.component';
-import { ProgramContactInformationComponent } from './authenticated/subforms/program-contact-information/program-contact-information.component';
-import { AdministrativeInformationComponent } from './authenticated/subforms/administrative-information/administrative-information.component';
 import { CgLiabilityComponent } from './authenticated/subforms/cg-liability/cg-liability.component';
-import { ProgramComponent } from './authenticated/subforms/program/program.component';
-import { ReviewApplicationComponent } from './authenticated/subforms/review-application/review-application.component';
-import { ProgramAuthorizerComponent } from './authenticated/subforms/program-authorizer/program-authorizer.component';
-import { HoursComponent } from './authenticated/subforms/hours/hours.component';
-import { ExpenseTableComponent } from './authenticated/subforms/expense-table/expense-table.component';
-import { ContractTombstoneComponent } from './authenticated/subforms/contract-tombstone/contract-tombstone.component';
-import { OrganizationProfileBoxComponent } from './authenticated/components/organization-profile-box/organization-profile-box.component';
 import { ContactInformationComponent } from './authenticated/subforms/contact-information/contact-information.component';
-import { AddressComponent } from './authenticated/subforms/address/address.component';
+import { ContractTombstoneComponent } from './authenticated/subforms/contract-tombstone/contract-tombstone.component';
+import { DashboardComponent } from './authenticated/dashboard/dashboard.component';
+import { ExpenseReportComponent } from './authenticated/expense-report/expense-report.component';
+import { ExpenseTableComponent } from './authenticated/subforms/expense-table/expense-table.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HoursComponent } from './authenticated/subforms/hours/hours.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { MinistryContactBoxComponent } from './authenticated/components/ministry-contact-box/ministry-contact-box.component';
+import { NgModule } from '@angular/core';
+import { OrganizationProfileBoxComponent } from './authenticated/components/organization-profile-box/organization-profile-box.component';
+import { PersonCardComponent } from './authenticated/subforms/person-card/person-card.component';
+import { PersonEditorComponent } from './authenticated/subforms/person-editor/person-editor.component';
 import { PersonPickerComponent } from './authenticated/subforms/person-picker/person-picker.component';
 import { PersonPickerListComponent } from './authenticated/subforms/person-picker-list/person-picker-list.component';
-import { MinistryContactBoxComponent } from './authenticated/components/ministry-contact-box/ministry-contact-box.component';
-import { PersonEditorComponent } from './authenticated/subforms/person-editor/person-editor.component';
-import { TestComponent } from './test/test.component';
-import { ProgramBudgetComponent } from './authenticated/subforms/program-budget/program-budget.component';
-import { RevenueSourceTableComponent } from './authenticated/subforms/revenue-source-table/revenue-source-table.component';
-import { TaskListComponent } from './authenticated/components/task-list/task-list.component';
-import { PersonCardComponent } from './authenticated/subforms/person-card/person-card.component';
-import { Address2Component } from './authenticated/subforms/address2/address2.component';
 import { PersonTableComponent } from './authenticated/subforms/person-table/person-table.component';
+import { PersonnelComponent } from './authenticated/personnel/personnel.component';
+import { ProfileComponent } from './authenticated/profile/profile.component';
+import { ProgramApplicationComponent } from './authenticated/program-application/program-application.component';
+import { ProgramAuthorizerComponent } from './authenticated/subforms/program-authorizer/program-authorizer.component';
+import { ProgramBudgetComponent } from './authenticated/subforms/program-budget/program-budget.component';
+import { ProgramComponent } from './authenticated/subforms/program/program.component';
+import { ProgramContactInformationComponent } from './authenticated/subforms/program-contact-information/program-contact-information.component';
 import { ProgramSummaryTableComponent } from './authenticated/subforms/program-summary-table/program-summary-table.component';
+import { RevenueSourceTableComponent } from './authenticated/subforms/revenue-source-table/revenue-source-table.component';
+import { ReviewApplicationComponent } from './authenticated/subforms/review-application/review-application.component';
+import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
+import { StatusReportComponent } from './authenticated/status-report/status-report.component';
+import { TaskListComponent } from './authenticated/components/task-list/task-list.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
+    Address2Component,
+    AddressComponent,
     AdministrativeInformationComponent,
     AppComponent,
     BudgetProposalComponent,
     CgLiabilityComponent,
+    ContactInformationComponent,
     ContractTombstoneComponent,
     DashboardComponent,
     ExpenseReportComponent,
     ExpenseTableComponent,
     HoursComponent,
     LandingPageComponent,
+    MinistryContactBoxComponent,
     OrganizationProfileBoxComponent,
+    PersonCardComponent,
+    PersonEditorComponent,
+    PersonPickerComponent,
+    PersonPickerListComponent,
+    PersonTableComponent,
     PersonnelComponent,
     ProfileComponent,
     ProgramApplicationComponent,
     ProgramAuthorizerComponent,
+    ProgramBudgetComponent,
     ProgramComponent,
     ProgramContactInformationComponent,
+    ProgramSummaryTableComponent,
+    RevenueSourceTableComponent,
     ReviewApplicationComponent,
     StatusReportComponent,
-    ContactInformationComponent,
-    AddressComponent,
-    PersonPickerComponent,
-    PersonPickerListComponent,
-    MinistryContactBoxComponent,
-    PersonEditorComponent,
-    TestComponent,
-    ProgramBudgetComponent,
-    RevenueSourceTableComponent,
     TaskListComponent,
-    PersonCardComponent,
-    Address2Component,
-    PersonTableComponent,
-    ProgramSummaryTableComponent,
+    TestComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     RouterModule,
     SharedModule
   ],
