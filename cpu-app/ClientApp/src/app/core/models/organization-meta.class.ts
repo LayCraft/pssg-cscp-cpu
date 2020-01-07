a5a52facc500eefbe5b116695ddca644b8ce8657
Curtis <curtis.laycraft@zoho.com>
2020-01-07 10:26:59 -0800

    remove old style objects

--- cpu-app/ClientApp/src/app/core/models/organization-meta.class.ts
+++ cpu-app/ClientApp/src/app/core/models/organization-meta.class.ts
@@ -1,16 +1,8 @@
-import { iContactInformation, ContactInformation } from './contact-information.class';
+import { iContactInformation } from './contact-information.class';
 
 export interface iOrganizationMeta {
   accountId: string; // this is the ID to identify an organization in dynamics. NOT A BCEID
   contactInformation: iContactInformation;
-  contracts: string[];
-  organizationId: string;
-  organizationName: string;
-  userId: string;
-}
-export class OrganizationMeta implements iOrganizationMeta {
-  accountId: string; // this is the ID to identify an organization in dynamics. NOT A BCEID
-  contactInformation: iContactInformation = new ContactInformation();
   contracts: string[];
   organizationId: string;
   organizationName: string;
