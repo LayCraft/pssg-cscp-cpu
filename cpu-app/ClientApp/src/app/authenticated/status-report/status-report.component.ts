af974ec46526a093961cfd5f4f36c5732875d840
Curtis <curtis.laycraft@zoho.com>
2020-01-07 09:23:19 -0800

    set up and test service

--- cpu-app/ClientApp/src/app/authenticated/status-report/status-report.component.ts
+++ cpu-app/ClientApp/src/app/authenticated/status-report/status-report.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { iContract } from '../../core/models/contract';
 import { IconStepperService, iStepperElement } from '../../shared/icon-stepper/icon-stepper.service';
 import { StateService } from '../../core/services/state.service';
+import { StatusReportService } from '../../core/services/status-report.service';
 
 @Component({
   selector: 'app-status-report',
@@ -10,43 +11,33 @@ import { StateService } from '../../core/services/state.service';
   styleUrls: ['./status-report.component.css']
 })
 export class StatusReportComponent implements OnInit {
-  contract: iContract;
+  response: any;
   // used for the stepper component
   stepperElements: iStepperElement[];
   currentStepperElement: iStepperElement;
   constructor(
     private stepperService: IconStepperService,
-    private stateService: StateService,
-    private route: ActivatedRoute,
+    private statusReportService: StatusReportService,
   ) { }
 
   ngOnInit() {
-    this.route.params.subscribe(p => {
-      // collect the contract from the route
-      const contractId = p['contractId'];
-      // don't subscribe because this is only used at page load to collect meta
-      const contracts: iContract[] = this.stateService.main.getValue().contracts;
-      // isolate the correct contract
-      for (let contract of contracts) {
-        if (contract.contractId == contractId) {
-          this.contract = contract;
-        }
-      }
-      // clear all of the old stepper elements
-      this.stepperService.reset();
-      this.constructDefaultstepperElements();
-    });
+    this.statusReportService.getStatusReportQuestions('fd889a40-14b2-e811-8163-480fcff4f621', '9e9b5111-51c9-e911-b80f-00505683fbf4', '0e309304-c4e6-e911-b811-00505683fbf4').subscribe(r => this.response = r);
+    // clear all of the old stepper elements
+    this.constructDefaultstepperElements();
     // stay in sync with
     this.stepperService.currentStepperElement.subscribe(e => this.currentStepperElement = e);
     this.stepperService.stepperElements.subscribe(e => this.stepperElements = e);
   }
+
   isCurrentStepperElement(item: iStepperElement): boolean {
     if (item.id === this.currentStepperElement.id) {
       return true;
     }
     return false;
   }
+
   constructDefaultstepperElements() {
+    this.stepperService.reset();
     // write the default beginning
     [
       {
