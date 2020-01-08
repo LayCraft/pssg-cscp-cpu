323c182ae5292d8e81953f26eacc38615051c531
Curtis <curtis.laycraft@zoho.com>
2020-01-08 10:46:43 -0800

    add models

--- cpu-app/Models/DynamicsMonthlyStatisticsAnswers.cs
+++ cpu-app/Models/DynamicsMonthlyStatisticsAnswers.cs
@@ -7,14 +7,10 @@ namespace Gov.Cscp.VictimServices.Public.Models
 {
 	public class DynamicsMonthlyStatisticsAnswers
 	{
-		// this is the model that Dynamics expects back to update the organization level information
 		public string BusinessBCeID { get; set; }
 		public string UserBCeID { get; set; }
 		public int ReportingPeriod { get; set; }
-
-
-		// public DynamicsDataCollectionLineItemCollection[] ScheduleGCollection { get; set; }
-		// public DynamicsScheduleGLineItemCollection[] ScheduleGLineItemCollection { get; set; }
+		public DynamicsDataCollectionLineItem[] AnswerCollection { get; set; }
 
 	}
 }
