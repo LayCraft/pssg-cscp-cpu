f0ba54223fd054ebcbadb6b1ea93b2c4b7faff9d
Curtis <curtis.laycraft@zoho.com>
2020-01-14 16:22:46 -0800

    cleanup

--- cpu-app/ClientApp/src/app/test/test.component.ts
+++ cpu-app/ClientApp/src/app/test/test.component.ts
@@ -8,15 +8,15 @@ import { TransmogrifierBudgetProposal } from '../core/models/transmogrifier-budg
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  data: any;
-  trans: TransmogrifierBudgetProposal
+  // data: any;
+  // trans: TransmogrifierBudgetProposal
   constructor(
-    private budgetProposalService: BudgetProposalService
+    // private budgetProposalService: BudgetProposalService
   ) { }
   ngOnInit() {
-    this.budgetProposalService.getBudgetProposal('fd889a40-14b2-e811-8163-480fcff4f621', '9e9b5111-51c9-e911-b80f-00505683fbf4', '47e47399-3633-ea11-b814-00505683fbf4').subscribe(p => {
-      this.data = p;
-      this.trans = new TransmogrifierBudgetProposal(p);
-    })
+    // this.budgetProposalService.getBudgetProposal('fd889a40-14b2-e811-8163-480fcff4f621', '9e9b5111-51c9-e911-b80f-00505683fbf4', '47e47399-3633-ea11-b814-00505683fbf4').subscribe(p => {
+    //   this.data = p;
+    //   this.trans = new TransmogrifierBudgetProposal(p);
+    // })
   }
 }
