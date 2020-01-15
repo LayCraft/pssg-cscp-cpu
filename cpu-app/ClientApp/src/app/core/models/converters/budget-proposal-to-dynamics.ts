a41500831f036ea4e5683102a8651ebcf9bfa61b
Curtis <curtis.laycraft@zoho.com>
2020-01-15 09:06:08 -0800

    add functions for conversion

---
+++ cpu-app/ClientApp/src/app/core/models/converters/budget-proposal-to-dynamics.ts
@@ -0,0 +1,4 @@
+export function convertBudgetProposalToDynamics(userId: string, organizationId: string, expenseReportId: string, e: any): any {
+  console.log('No conversion of budget proposal yet.', e);
+  return null;
+}
---
+++ cpu-app/ClientApp/src/app/core/models/converters/program-application-to-dynamics.ts
@@ -0,0 +1,4 @@
+export function convertProgramApplicationToDynamics(userId: string, organizationId: string, contractId: string, e: any): any {
+  console.log('No conversion of program application yet.', e);
+  return null;
+}
