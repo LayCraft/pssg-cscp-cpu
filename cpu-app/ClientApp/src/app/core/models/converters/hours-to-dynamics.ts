7111b8106216f8ee62612b476a40980016850877
Curtis <curtis.laycraft@zoho.com>
2020-01-21 11:17:48 -0800

    moment normalize hours out

--- cpu-app/ClientApp/src/app/core/models/converters/hours-to-dynamics.ts
+++ cpu-app/ClientApp/src/app/core/models/converters/hours-to-dynamics.ts
@@ -1,12 +1,13 @@
 import { iHours } from "../hours.interface";
 import { iDynamicsSchedule } from "../dynamics-blob";
 import { encodeToWeekDayCodes } from "../../constants/encode-to-week-days";
+import * as moment from 'moment';
 
 export function convertHoursToDynamics(hours: iHours, programId: string, standByHours = false): iDynamicsSchedule {
-
+  console.log(hours.open);
   return {
-    vsd_scheduledendtime: "17:00",
-    vsd_scheduledstarttime: "8:00",
+    vsd_scheduledendtime: moment(hours.closed).format('HH:mm'),
+    vsd_scheduledstarttime: moment(hours.open).format('HH:mm'),
     vsd_days: encodeToWeekDayCodes(hours),
     vsd_scheduleid: hours.hoursId,
     _vsd_programid_value: programId,
