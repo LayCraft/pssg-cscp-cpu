f8d6aae696e0c63276ba5260762fe1faa6824719
Curtis <curtis.laycraft@zoho.com>
2020-01-29 13:10:54 -0800

    cleanup

--- cpu-app/Infrastructure/ApplicationType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gov.Cscp.VictimServices.Public.Infrastructure
-{
-    public enum ApplicationType
-    {
-        Victim = 100000002,
-        ImmediateFamilyMember = 100000001,
-        Witness = 100000000
-    }
-}
