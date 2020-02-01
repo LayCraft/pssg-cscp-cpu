365d66852d4195e236500e70a4341755581dca04
Curtis <curtis.laycraft@zoho.com>
2020-01-31 16:35:39 -0800

    update file interface

--- cpu-app/ClientApp/src/app/core/models/dynamics-file.interface.ts
+++ cpu-app/ClientApp/src/app/core/models/dynamics-file.interface.ts
@@ -9,3 +9,15 @@ export interface iDynamicsDocument {
   filename: string;
   body: string;
 }
+
+export interface iDynamicsFilePost {
+  IsSuccess?: boolean;
+  Result?: string;
+  Businessbceid: string;
+  Userbceid: string;
+  DocumentCollection?: iDynamicsDocumentPost[];
+}
+export interface iDynamicsDocumentPost {
+  filename: string;
+  body: string;
+}
