0c65eb0bd595e7742f67bfc339b1ff8f63414f93
Curtis <curtis.laycraft@zoho.com>
2020-02-03 15:00:29 -0800

    additional properties

--- cpu-app/Models/program-application/DynamicsProgramApplicationContractPost.cs
+++ cpu-app/Models/program-application/DynamicsProgramApplicationContractPost.cs
@@ -7,15 +7,14 @@ namespace Gov.Cscp.VictimServices.Public.Models
 {
 	public class DynamicsProgramApplicationContractPost
 	{
-
+		private string _vsd_ContactLookup1fortunecookiebind;
+		private string _vsd_ContactLookup2fortunecookiebind;
 		public string fortunecookietype
 		{
 			get { return "#Microsoft.Dynamics.CRM.vsd_contract"; }
 		}
-		private string _vsd_ContactLookup1fortunecookiebind;
 		public string vsd_ContactLookup1fortunecookiebind
 		{
-			// executive contact on schedule f
 			get
 			{
 				if (_vsd_ContactLookup1fortunecookiebind != null)
@@ -29,11 +28,8 @@ namespace Gov.Cscp.VictimServices.Public.Models
 			}
 			set { _vsd_ContactLookup1fortunecookiebind = value; }
 		}
-
-		private string _vsd_ContactLookup2fortunecookiebind;
 		public string vsd_ContactLookup2fortunecookiebind
 		{
-			// executive contact on schedule f
 			get
 			{
 				if (_vsd_ContactLookup2fortunecookiebind != null)
@@ -47,12 +43,13 @@ namespace Gov.Cscp.VictimServices.Public.Models
 			}
 			set { _vsd_ContactLookup2fortunecookiebind = value; }
 		}
-
-		public string _vsd_contactlookup1_value { get; set; }
-		public string _vsd_contactlookup2_value { get; set; }
-		public string _vsd_customer_value { get; set; }
 		public string vsd_contractid { get; set; }
 		public string vsd_cpu_specificunion { get; set; }
 		public string vsd_name { get; set; }
+		public bool vsd_cpu_memberofcssea { get; set; }
+		public bool vsd_cpu_staffunionized { get; set; }
+		public string vsd_cpu_humanresourcepolicies { get; set; }
+		public string vsd_cpu_insuranceoptions { get; set; }
+		public string vsd_cpu_programstaffsubcontracted { get; set; }
 	}
 }
