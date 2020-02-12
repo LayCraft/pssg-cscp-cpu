e12240c2beeb2ebec4872d48702560e0ad5ca94d
Joseph Weinkam <jweinkam@filmrobot.com>
2020-02-12 11:39:45 -0800

    Update display name for status_report. Remove some test console logs. Revert some of the null changes for now and will address those later.

--- cpu-app/ClientApp/src/app/core/constants/form-type-dict.ts
+++ cpu-app/ClientApp/src/app/core/constants/form-type-dict.ts
@@ -3,7 +3,7 @@ export const formTypeDict = {
   '315b18c9-e8f5-e911-b811-00505683fbf4': ['315b18c9-e8f5-e911-b811-00505683fbf4', 'Modification Agreement'],
   '50eeb9ba-e8f5-e911-b811-00505683fbf4': ['50eeb9ba-e8f5-e911-b811-00505683fbf4', 'Program Surplus'],
   '768faf46-e8f5-e911-b811-00505683fbf4': ['budget_proposal', 'Annual budget proposal'],
-  'c84daf8d-e8f5-e911-b811-00505683fbf4': ['status_report', 'Monthly Statistics Report'],
+  'c84daf8d-e8f5-e911-b811-00505683fbf4': ['status_report', 'Statistics report'],
   '9d0ef880-e8f5-e911-b811-00505683fbf4': ['expense_report', 'Quarterly expense report'],
   '47525432-e8f5-e911-b811-00505683fbf4': ['program_application', 'Annual program application'],
   '1581f216-5d48-ea11-b816-00505683fbf4': ['sign_contract', 'Sign contract']
--- cpu-app/Models/program-application/DynamicsProgramApplicationContractPost.cs
+++ cpu-app/Models/program-application/DynamicsProgramApplicationContractPost.cs
@@ -42,7 +42,7 @@ namespace Gov.Cscp.VictimServices.Public.Models
 		public bool vsd_cpu_programstaffsubcontracted { get; set; }
 		public bool vsd_cpu_staffunionized { get; set; }
 		public int vsd_cpu_insuranceoptions { get; set; }
-		public int? vsd_cpu_memberofcssea { get; set; }
+		public int vsd_cpu_memberofcssea { get; set; }
 		public string fortunecookietype { get { return "Microsoft.Dynamics.CRM.vsd_contract"; } }
 		public string vsd_contractid { get; set; }
 		public string vsd_cpu_humanresourcepolices { get; set; }
