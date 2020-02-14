8b09ebf48dacdaaa3ed67547a9e005d6db80b143
Joseph Weinkam <jweinkam@filmrobot.com>
2020-02-14 12:57:37 -0800

    add a scraper function to update the modelString to handle null lookup values before making the API request. At the moment it's just on the Program Application Controller, but will move it to a general helper section and use it for more Controllers

--- cpu-app/Models/program-application/DynamicsProgramApplicationContractPost.cs
+++ cpu-app/Models/program-application/DynamicsProgramApplicationContractPost.cs
@@ -41,8 +41,8 @@ namespace Gov.Cscp.Victims.Public.Models
 		}
 		public bool vsd_cpu_programstaffsubcontracted { get; set; }
 		public bool vsd_cpu_staffunionized { get; set; }
-		public int vsd_cpu_insuranceoptions { get; set; }
-		public int vsd_cpu_memberofcssea { get; set; }
+		public int? vsd_cpu_insuranceoptions { get; set; }
+		public int? vsd_cpu_memberofcssea { get; set; }
 		public string fortunecookietype { get { return "Microsoft.Dynamics.CRM.vsd_contract"; } }
 		public string vsd_contractid { get; set; }
 		public string vsd_cpu_humanresourcepolices { get; set; }
