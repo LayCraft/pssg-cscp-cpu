62ff9ae51c72957b28c4f4a664e2b43c7b850a1c
Joseph Weinkam <jweinkam@filmrobot.com>
2020-03-02 12:50:53 -0800

    fix missing window variable on login redirect

--- cpu-app/ClientApp/src/app/landing-page/landing-page.component.ts
+++ cpu-app/ClientApp/src/app/landing-page/landing-page.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
+  window = window;
   constructor(private router: Router) { }
 
   ngOnInit() {
