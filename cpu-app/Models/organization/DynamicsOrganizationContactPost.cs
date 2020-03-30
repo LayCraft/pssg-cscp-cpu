5254b167eeff2b1ceec5a1b255b29f438d82b2d6
Joseph Weinkam <jweinkam@filmrobot.com>
2020-03-30 14:24:14 -0700

    hookup employment status on personnel page. Some style tweaks.

--- cpu-app/Models/organization/DynamicsOrganizationContactPost.cs
+++ cpu-app/Models/organization/DynamicsOrganizationContactPost.cs
@@ -23,6 +23,7 @@ namespace Gov.Cscp.Victims.Public.Models
 		public string lastname { get; set; }
 		public string middlename { get; set; }
 		public string mobilephone { get; set; }
+		public int? vsd_employmentstatus {get; set;}
 		public string fortunecookietype { get { return "Microsoft.Dynamics.CRM.contact"; } }
 		public string vsd_bceid { get; set; }
 		// statecode is an optional status value. 1 is soft delete and null shouldn't be included.
