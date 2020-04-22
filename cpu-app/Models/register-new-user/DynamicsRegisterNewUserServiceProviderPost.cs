193bbcc3e7fb890eb2e07277c5ebd00ffb7ad6a6
Joseph Weinkam <jweinkam@filmrobot.com>
2020-04-22 11:05:46 -0700

    add fields for service provider post on controller

--- cpu-app/Models/register-new-user/DynamicsRegisterNewUserServiceProviderPost.cs
+++ cpu-app/Models/register-new-user/DynamicsRegisterNewUserServiceProviderPost.cs
@@ -9,5 +9,14 @@ namespace Gov.Cscp.Victims.Public.Models
     {
         public string fortunecookietype { get { return "Microsoft.Dynamics.CRM.account"; } }
         public string name { get; set; }
+        public string address1_city { get; set; }
+        public string address1_composite { get; set; }
+        public string address1_line1 { get; set; }
+        public string address1_line2 { get; set; }
+        public string address1_postalcode { get; set; }
+        public string address1_stateorprovince { get; set; }
+        public string emailaddress1 { get; set; }
+        public string fax { get; set; }
+        public string mobilephone { get; set; }
     }
 }
