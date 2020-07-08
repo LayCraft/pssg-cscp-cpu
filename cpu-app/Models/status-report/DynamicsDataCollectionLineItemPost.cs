cd4cb345f7e8da902f329aa2813fe45b90be0fcd
Joseph Weinkam <jweinkam@filmrobot.com>
2020-07-08 13:22:42 -0700

    add new id fields to status report answer collection

--- cpu-app/Models/status-report/DynamicsDataCollectionLineItemPost.cs
+++ cpu-app/Models/status-report/DynamicsDataCollectionLineItemPost.cs
@@ -18,5 +18,41 @@ namespace Gov.Cscp.Victims.Public.Models
         public int vsd_questionorder { get; set; }
         public string vsd_textanswer { get; set; }
         public int? vsd_yesno { get; set; }
+
+        private string _vsd_QuestionIdfortunecookiebind;
+        public string vsd_QuestionIdfortunecookiebind
+        {
+            get
+            {
+                if (_vsd_QuestionIdfortunecookiebind != null)
+                {
+                    return "/vsd_cpustatisticsmasterdatas(" + _vsd_QuestionIdfortunecookiebind + ")";
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set { _vsd_QuestionIdfortunecookiebind = value; }
+        }
+
+        private string _vsd_CategoryIdfortunecookiebind;
+        public string vsd_CategoryIdfortunecookiebind
+        {
+            get
+            {
+                if (_vsd_CategoryIdfortunecookiebind != null)
+                {
+                    return "/vsd_monthlystatisticscategories(" + _vsd_CategoryIdfortunecookiebind + ")";
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            set { _vsd_CategoryIdfortunecookiebind = value; }
+        }
+
+
     }
 }
